1bb6449fb7846383a1e9da8afa1775b2823b11e8
Minuk_LEE <74412438+MinukLee1@users.noreply.github.com>
2022-05-24 21:11:05 +0900

    Update p2d_token_reward.sol

--- contracts/p2d_token_reward.sol
+++ contracts/p2d_token_reward.sol
@@ -8,28 +8,26 @@ contract P2DToken is ERC20 {
     uint maxTopScores = 5;
     uint public amount = 10**18 ;
     uint public score;
-
     address payable public _addr = payable(0xC0c9093Df0fefd899f82aE6735C2e0730eD9D428);
 
     struct TopScore{
         address addr;
         uint256 score;
     }
-
     TopScore[] public topScores;
 
     mapping(address => uint256) public _userscore;
-    mapping(address => uint256) private _balances;
 
     constructor() ERC20("PlaytoDonate", "P2D") {}
 
         // token reward
         function reward(uint _score) public {
-            require(_score >= amount);
+        require(_score >= amount);
 
+        
         score = _score / (10**18);
-        _mint(msg.sender, score);
-        _userscore[msg.sender] += score;
+        _mint(msg.sender, _score);
+        _userscore[msg.sender] += _score;
 
         TopScore memory topScore = TopScore(msg.sender, score);
         topScores.push(topScore);
@@ -45,17 +43,14 @@ contract P2DToken is ERC20 {
             }
         }
     }
-
     // 배열의 특정 값과 for 문을 이용하여 배열의 전체 값을 불러 오기 위해 _number 라는 파라미터를 입력 받는다.
     function getUserScore(uint _number) public view returns(address user_addr, uint user_score){
         user_addr = topScores[_number].addr;
         user_score = topScores[_number].score;
     }
-
     // token transfer
     function send(uint256 _value) payable public {
     require(_value >= amount);
-    require(_value < _balances[msg.sender]);
     ERC20.transfer(_addr, _value);
     }
 }
