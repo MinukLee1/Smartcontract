b4c64ae119e98b9c217b51b8352b37d82ec78dbb
Minuk_LEE <74412438+MinukLee1@users.noreply.github.com>
2022-06-02 00:15:05 +0900

    Update p2d_token_reward.sol

--- contracts/p2d_token_reward.sol
+++ contracts/p2d_token_reward.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract P2DToken is ERC20 {
 
     uint maxTopScores = 5;
-    uint public amount = 10**18 ;
+    uint public amount = 1018 ;
     uint public score;
     address payable public _addr = payable(0xC0c9093Df0fefd899f82aE6735C2e0730eD9D428);
 
@@ -17,17 +17,19 @@ contract P2DToken is ERC20 {
     TopScore[] public topScores;
 
     mapping(address => uint256) public _userscore;
+    mapping(address => uint256) public _accumulate;
 
     constructor() ERC20("PlaytoDonate", "P2D") {}
 
-        // token reward
-        function reward(uint _score) public {
-        require(_score >= amount);
+    // token reward
+    function reward(uint _score) public {
+    require(_score >= amount);
 
-        
-        score = _score / (10**18);
+         score = _score / (1018);
         _mint(msg.sender, _score);
-        _userscore[msg.sender] += _score;
+
+        _userscore[msg.sender] = _score;
+        _accumulate[msg.sender] += _score;
 
         TopScore memory topScore = TopScore(msg.sender, score);
         topScores.push(topScore);
@@ -43,14 +45,16 @@ contract P2DToken is ERC20 {
             }
         }
     }
-    // 배열의 특정 값과 for 문을 이용하여 배열의 전체 값을 불러 오기 위해 _number 라는 파라미터를 입력 받는다.
-    function getUserScore(uint _number) public view returns(address user_addr, uint user_score){
-        user_addr = topScores[_number].addr;
-        user_score = topScores[_number].score;
-    }
+
     // token transfer
     function send(uint256 _value) payable public {
     require(_value >= amount);
     ERC20.transfer(_addr, _value);
+    }
+
+    // 배열의 특정 값과 for 문을 이용하여 배열의 전체 값을 불러 오기 위해 _number 라는 파라미터를 입력 받는다.
+    function getUserScore(uint _number) public view returns(address user_addr, uint user_score){
+        user_addr = topScores[_number].addr;
+        user_score = topScores[_number].score;
     }
 }
