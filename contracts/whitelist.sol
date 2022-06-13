5dcc77ee9ca1c6cfedc06b55e635ecf2b1f2a8e5
Minuk_LEE <74412438+MinukLee1@users.noreply.github.com>
2022-06-14 02:02:44 +0900

    Update whitelist.sol

--- contracts/whitelist.sol
+++ contracts/whitelist.sol
@@ -1,1 +1,35 @@
-whitelist solidity code
+
+pragma solidity ^0.4.19;
+import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+
+contract Whitelist is Ownable {
+    mapping(address => bool) whitelist;
+    event AddedToWhitelist(address indexed account);
+    event RemovedFromWhitelist(address indexed account);
+
+    modifier onlyWhitelisted() {
+        require(isWhitelisted(msg.sender));
+        _;
+    }
+
+    function add(address _address) public onlyOwner {
+        whitelist[_address] = true;
+        emit AddedToWhitelist(_address);
+    }
+
+    function remove(address _address) public onlyOwner {
+        whitelist[_address] = false;
+        emit RemovedFromWhitelist(_address);
+    }
+
+    function isWhitelisted(address _address) public view returns(bool) {
+        return whitelist[_address];
+    }
+}
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+C
